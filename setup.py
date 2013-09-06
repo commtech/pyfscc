2b9b08bd3d6e14358777a2eef7be928bf83d351c
William Fagan <willf@commtech-fastcom.com>
2013-09-06 12:19:04 -0500

    Deploy DLL's along with fscc.py.

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from distutils.core import setup
 
 if os.name == 'nt':
     requirements = ['serial', 'win32']
+    data_files=[('fscc', ['cfscc.dll'])]
 else:
     requirements = ['serial']
+    data_files=[('fscc', ['libcfscc.so'])]
 
 setup(name='pyfscc',
       version='1.0.0',
       py_modules=['fscc'],
+      data_files=data_files,
       requires=requirements,
       )
