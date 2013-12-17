7bbf1eace6ca59ff9727b503925f25e8cd92dd27
William Fagan <willf@commtech-fastcom.com>
2013-12-17 11:39:51 -0600

    Use absolute path on list_ports so installs can find the files.

--- fscc/tools/list_ports.py
+++ fscc/tools/list_ports.py
@@ -2,9 +2,9 @@ import os
 
 # chose an implementation, depending on os
 if os.name == 'nt':
-    from list_ports_windows import *
+    from fscc.tools.list_ports_windows import *
 elif os.name == 'posix':
-    from list_ports_linux import *
+    from fscc.tools.list_ports_linux import *
 
 if __name__ == '__main__':
     print(sorted(fsccports()))
